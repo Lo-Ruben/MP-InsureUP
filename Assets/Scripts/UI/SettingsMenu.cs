1ce892dd24e2db7595488ca62f0ed109f7b29ae6
SpaceCowMoo <2100419G@student.tp.edu.sg>
2023-08-10 16:21:12 +0800

    Settings Fix Slider
    -Slider position properly represents volume

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-
+    float volumeFloat;
+    public Slider SliderUI;
     public AudioMixer musicVolume;
-    public void SetVolume(float volume)
-     {
-            Debug.Log(volume);
-            musicVolume.SetFloat("volume", volume);
 
-           
-      }
+    public void SetVolume(float volume)
+    {
+        musicVolume.SetFloat("volume", volume);
+    }
+
+    private void OnEnable()
+    {
+        musicVolume.GetFloat("volume", out volumeFloat);
+        SliderUI.value = volumeFloat;
+    }
 
 
 }
